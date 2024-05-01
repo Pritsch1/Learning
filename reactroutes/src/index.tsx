c90d8f29b55437faab51222fe1907ad27942ef71
Pritsch <pritschdec@gmail.com>
2024-05-01 14:10:38 -0300

    forgot to commit...

--- reactroutes/src/index.tsx
+++ reactroutes/src/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root, { loader as root_loader, action as root_action } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, { loader as contact_loader, action as contact_action } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index_contact";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: root_loader,
+        action: root_action,
+        children: [
+            {
+                errorElement: <ErrorPage />,
+                children: [
+                    { index: true, element: <Index /> },
+                    {
+                        path: "contacts/:contactId",
+                        element: <Contact />,
+                        loader: contact_loader,
+                        action: contact_action,
+                    },
+                    {
+                        path: "contacts/:contactId/edit",
+                        element: <EditContact />,
+                        loader: contact_loader,
+                        action: editAction,
+                    },
+                    {
+                        path: "contacts/:contactId/destroy",
+                        action: destroyAction,
+                        errorElement: <div>Oops! There was an error.</div>,
+                    },
+                ]
+            }
+        ],
+    },
+]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
